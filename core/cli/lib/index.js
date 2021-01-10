aa56ff3b2924cb58ed59655d888d7608d08ea0ac
TaroSunn <sunxiaomingyes@gmail.com>
2021-01-10 17:37:22 +0800

    feat: Node版本检查功能

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,11 +2,29 @@
 
 module.exports = index;
 
+const log = require('@moleculeblock/cli-log');
+const semver = require('semver')
+const colors = require('colors/safe')
 const pkg = require('../package.json')
-const log = require('@moleculeblock/cli-log')
+const { LOWEST_NODE_VERSION } = require('./const');
+
 
 function index() {
-  checkPkgVersion()
+  try {
+    checkPkgVersion()
+    checkNodeVersion()
+  } catch (e) {
+    log.error(e.message)
+  }
+}
+
+function checkNodeVersion() {
+  const currentVersion = process.version // 当前node版本号
+  const lowestVersion = LOWEST_NODE_VERSION // 最低版本号
+  
+  if(!semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(colors.red(`moleculeblock-cli 只支持 NodeJS v${lowestVersion} 以上版本`))
+  }
 }
 
 function checkPkgVersion() {
